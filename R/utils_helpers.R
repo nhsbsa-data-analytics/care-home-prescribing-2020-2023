271cb075cccdb5e4d0b74662cef7b06ce0e991d4
mgarbuzov <92371047+mgarbuzov@users.noreply.github.com>
2023-05-16 16:10:53 +0100

    theme_nhsbsa() consistent with Pt1

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -9,29 +9,33 @@
 #' @return
 #' @export
 theme_nhsbsa <- function(hc, palette = NA, stack = "normal") {
-
+  
+  # Set the thousands seperator
+  hcoptslang <- getOption("highcharter.lang")
+  hcoptslang$thousandsSep <- ","
+  options(highcharter.lang = hcoptslang)
+  
   # Load theme from nhsbsaR package
   theme_nhsbsa_hc <- nhsbsaR::theme_nhsbsa_hc(family = "Frutiger W01")
-
+  
   # Add the plot options
   theme_nhsbsa_hc$plotOptions <- list(
     series = list(stacking = stack, borderWidth = 0),
     bar = list(groupPadding = 0.1)
   )
-
-  # Add the palettes (hack the highlight palette to have a lighter grey)
+  
+  # Add the palettes
   theme_nhsbsa_hc$colors <- nhsbsaR::palette_nhsbsa(palette = palette)
-  theme_nhsbsa_hc$colors[theme_nhsbsa_hc$colors == "#768692"] <- "#d1d5d6"
   theme_nhsbsa_hc$colAxis <- list(
     min = 0,
     minColor = nhsbsaR::palette_nhsbsa(palette = "gradient")[1],
     maxColor = nhsbsaR::palette_nhsbsa(palette = "gradient")[2]
   )
-
+  
   # Style based on the NHS frontend toolkit
   theme_nhsbsa_hc$xAxis$className <- "nhsuk-body-s"
   theme_nhsbsa_hc$yAxis$className <- "nhsuk-body-s"
-
+  
   # Add the theme to the chart and then remove the credits afterwards (currently
   # does not work to do this within the theme)
   hc %>%
