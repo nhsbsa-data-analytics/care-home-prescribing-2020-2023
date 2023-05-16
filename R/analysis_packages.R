4aa6f483fdf30bfd340dfebc4d14ef3faab270ba
Mark McPherson <mark.mcpherson@nhsbsa.nhs.uk>
2023-05-16 14:02:30 +0000

    Fix all package setup errors & warnings + add gitleaks workflow

--- R/analysis_packages.R
+++ R/analysis_packages.R
@@ -1,30 +1,32 @@
-
-# Install pacman if necessary
-if (!('pacman' %in% rownames(installed.packages()))) {
-  install.packages('pacman')
+analysis_packages <- function() {
+  # Install pacman if necessary
+  if (!('pacman' %in% rownames(utils::installed.packages()))) {
+    utils::install.packages('pacman')
+  }
+  
+  # Libraries
+  pacman::p_load(
+    # Global
+    'dplyr',
+    'dbplyr',
+    'DBI',
+    'tictoc',
+    'parallel',
+    'janitor',
+    'yaml',
+    
+    # CQC API data 
+    'httr',
+    'jsonlite',
+    
+    # AddressBase Packages
+    'archive',
+    'readr',
+    'lubridate'
+  )
+  
+  # Libraries from github
+  pacman::p_load_gh("nhsbsa-data-analytics/nhsbsaR")
+  pacman::p_load_gh("nhsbsa-data-analytics/addressMatchR")
 }
 
-# Libraries
-pacman::p_load(
-  # Global
-  'dplyr',
-  'dbplyr',
-  'DBI',
-  'tictoc',
-  'parallel',
-  'janitor',
-  'yaml',
-  
-  # CQC API data 
-  'httr',
-  'jsonlite',
-  
-  # AddressBase Packages
-  'archive',
-  'readr',
-  'lubridate'
-)
-
-# Libraries from github
-pacman::p_load_gh("nhsbsa-data-analytics/nhsbsaR")
-pacman::p_load_gh("nhsbsa-data-analytics/addressMatchR")
