80547e8f1e1ded31749470880566848fd47ef351
mgarbuzov <92371047+mgarbuzov@users.noreply.github.com>
2023-06-08 16:55:01 +0100

    First draft of data-raw script complete

--- data-raw/patients_age_gender_df.R
+++ data-raw/patients_age_gender_df.R
@@ -6,20 +6,21 @@ devtools::load_all()
 con <- nhsbsaR::con_nhsbsa(database = "DALP")
 
 # Item-level base table
-fact_db <- con |>
-  tbl(from = in_schema("MAMCP", "INT646_BASE_20210401_20220331"))
+base_db <- con |>
+  tbl(from = in_schema("DALL_REF", "INT646_BASE_20200401_20230331"))
  
 # Filter to care home only and add a dummy overall column
- fact_db <- fact_db %>%
-   filter(CH_FLAG == "Care home") %>%
+base_db <- base_db |>
+   filter(CH_FLAG == 1L) |>
    mutate(OVERALL = "Overall")
  
-# # Loop over each geography and aggregate
-for (geography_name in names(careHomePrescribing2023::geographys)) {
+# Loop over each geography and aggregate using purrr's map function approach
 
-  # Extract the geography cols
+patients_by_fy_geo_age_gender_fun <- function(geography_name) {
+  
+  # Identify geography cols
   geography_cols <-
-    careHomePrescribing2023::geographys[[geography_name]] %>%
+    carehomes2::geographys[[geography_name]] %>%
     purrr::set_names(
       nm = stringr::str_replace(
         string = names(.),
@@ -27,16 +28,17 @@ for (geography_name in names(careHomePrescribing2023::geographys)) {
         replacement = "GEOGRAPHY"
       )
     )
-
+  
   # Overall total patients
-  tmp_db <- fact_db %>%
+  base_db |>
     group_by(
+      FY,
       GEOGRAPHY = geography_name,
       across(all_of(unname(geography_cols))),
       GENDER,
       AGE_BAND
-    ) %>%
-    rename(!!!geography_cols) %>% #???
+    ) |>
+    rename(!!!geography_cols) |>
     mutate(
       # If the SUB_GEOGRAPHY_NAME is NA then set gender to NA
       GENDER = ifelse(is.na(SUB_GEOGRAPHY_NAME), NA, GENDER),
@@ -46,133 +48,68 @@ for (geography_name in names(careHomePrescribing2023::geographys)) {
         yes = NA,
         AGE_BAND
       )
-    ) %>%
-    summarise(TOTAL_PATIENTS = n_distinct(NHS_NO),
-              .groups = "drop") |>
-    mutate(PCT_PATIENTS = TOTAL_PATIENTS / sum(TOTAL_PATIENTS) * 100) %>%
+    ) |>
+    summarise(TOTAL_PATIENTS = n_distinct(NHS_NO)) |>
+    ungroup(GENDER, AGE_BAND, TOTAL_PATIENTS) |>
+    mutate(PCT_PATIENTS = TOTAL_PATIENTS / sum(TOTAL_PATIENTS) * 100) |>
+    ungroup() |>
+    nhsbsaR::collect_with_parallelism(32)
+  
+}
+
+patients_by_fy_geo_age_gender_df <- purrr::map(
+  names(carehomes2::geographys),
+  patients_by_fy_geo_age_gender_fun
+  ) |>
+  purrr::list_rbind()
+
+# Get all the possible combinations
+patients_by_fy_geo_age_gender_df <-
+  patients_by_fy_geo_age_gender_df |>
+  tidyr::complete(
+    # Only geographies that already exist
+    tidyr::nesting(FY, GEOGRAPHY, SUB_GEOGRAPHY_CODE, SUB_GEOGRAPHY_NAME),
+    # Only age band and gender combinations that exist (so we only have NA age
+    # band for NA genders)
+    tidyr::nesting(AGE_BAND, GENDER),
+    fill = list(
+      TOTAL_PATIENTS = 0L,
+      PCT_PATIENTS = 0
+    )
+  )
+
+# Apply SDC to total patients and percentage of patients
+patients_by_fy_geo_age_gender_df <-
+  patients_by_fy_geo_age_gender_df |>
+  mutate(
+    SDC = ifelse(TOTAL_PATIENTS %in% c(1, 2, 3, 4), 1, 0),
+    SDC_TOTAL_PATIENTS =
+      ifelse(SDC == 1, NA_integer_, round(TOTAL_PATIENTS, -1)),
+    SDC_PCT_PATIENTS =
+      ifelse(SDC == 1, NA_integer_, janitor::round_half_up(PCT_PATIENTS, 1))
+  ) |>
+  select(-SDC)
+
+# Format factors etc and sort
+patients_by_fy_geo_age_gender_df <-
+  patients_by_fy_geo_age_gender_df |>
+  carehomes2::format_data_raw(
+    vars = c("GENDER", "AGE_BAND")
+  )
 
 
-  # (merged into above)
-  # # Calculate the percentage
-  # tmp_db <- tmp_db %>%
-  #   ungroup(GENDER, AGE_BAND, TOTAL_PATIENTS) %>%
-  #   mutate(PCT_PATIENTS = TOTAL_PATIENTS / sum(TOTAL_PATIENTS) * 100) %>%
-  #   ungroup()
+# Clean PCN names
+patients_by_fy_geo_age_gender_df <-
+  patients_by_fy_geo_age_gender_df |>
+    mutate(SUB_GEOGRAPHY_NAME = stringr::str_remove(SUB_GEOGRAPHY_NAME, " PCN \\(as of \\d{6}\\)$")) |>
+    mutate(SUB_GEOGRAPHY_NAME = stringr::str_remove(SUB_GEOGRAPHY_NAME, " ICB$"))
+  
 
-    # (not needed if using pmap_dfr and collecting each loop, example below)
-#   # Either create the table or append to it
-#   if (geography_name == "Overall") {
-#     # On the first iteration initialise the table
-# 
-#     patients_by_geography_and_gender_and_age_band_db <- tmp_db
-#   } else {
-#     # Union results to initialised table
-# 
-#     patients_by_geography_and_gender_and_age_band_db <- union_all(
-#       x = patients_by_geography_and_gender_and_age_band_db,
-#       y = tmp_db
-#     )
-#   }
- }
+# Add to data/
+usethis::use_data(
+  patients_by_fy_geo_age_gender_df,
+  overwrite = T
+)
 
- 
-demographic_factor_summary <- function(period, breakdown_cols) {
-   
-   # calculate output count and total per breakdown_cols
-   
-   breakdown_cols <- c(
-     "PERIOD", "AFC_BAND", "PENSION_CONTRIBUTION_BAND",
-     "AGE_BAND", "CONTINENT", "CONTRACT_TYPE",
-     "FTE_CATEGORY", "DISABILITY_STATUS",
-     "IMD_QUINTILE", "PERSON_ETHNIC_5",
-     "GENDER", "SERVICE_YEARS_CATEGORY",
-     "STAFF_GROUP", "UK_NATIONAL_FLAG"
-   )
-   
-   tmp_db <- base_db |>
-     mutate(PERIOD = case_when(
-       period %in% c("2020/21", "2021/22", "2022/23") ~ FINANCIAL_YEAR,
-       period == latest_month ~ YEAR_MONTH
-     )) |>
-     filter(PERIOD == period) |>
-     group_by(
-       across(all_of(unname(breakdown_cols))),
-     ) |>
-     summarise(
-       OPTOUT_COUNT = sum(ifelse(PERSON_OPTOUT_DECISION == "Yes", 1, 0), na.rm = TRUE),
-       NEW_OPTOUT_COUNT = sum(ifelse(PERSON_LEAVER_FLAG == "Y", 1, 0), na.rm = TRUE),
-       TOTAL = n(),
-       .groups = "drop"
-     ) |>
-     collect()
-   
-   if (nrow(tmp_db) == 0) {
-     return(NULL)
-   } else {
-     return(tmp_db)
-   }
-   
- }
- 
- 
- demographic_factor_df <- tidyr::crossing(
-   period = analysis_period
- ) |>
-   purrr::pmap_dfr(demographic_factor_summary)
- 
- 
- 
- 
- 
-# 
-# # Collect
-# patients_by_geography_and_gender_and_age_band_df <-
-#   patients_by_geography_and_gender_and_age_band_db %>%
-#   # NOTE: Don't know why but collect won't work without this... throws error:
-#   # ORA-24374: define not done before fetch or execute and fetch
-#   # but works this filter (which actually filters nothing...)
-#   filter(!is.na(GEOGRAPHY)) %>%
-#   collect()
-# 
-# # Get all the possible combinations
-# patients_by_geography_and_gender_and_age_band_df <-
-#   patients_by_geography_and_gender_and_age_band_df %>%
-#   tidyr::complete(
-#     # Only geographies that already exist
-#     tidyr::nesting(GEOGRAPHY, SUB_GEOGRAPHY_CODE, SUB_GEOGRAPHY_NAME),
-#     # Only age band and gender combinations that exist (so we only have NA age
-#     # band for NA genders)
-#     tidyr::nesting(AGE_BAND, GENDER),
-#     fill = list(
-#       TOTAL_PATIENTS = 0L,
-#       PCT_PATIENTS = 0
-#     )
-#   )
-# 
-# # Apply SDC to total patients and percentage of patients
-# patients_by_geography_and_gender_and_age_band_df <-
-#   patients_by_geography_and_gender_and_age_band_df %>%
-#   mutate(
-#     SDC = ifelse(TOTAL_PATIENTS %in% c(1, 2, 3, 4), 1, 0),
-#     SDC_TOTAL_PATIENTS =
-#       ifelse(SDC == 1, NA_integer_, round(TOTAL_PATIENTS, -1)),
-#     SDC_PCT_PATIENTS =
-#       ifelse(SDC == 1, NA_integer_, janitor::round_half_up(PCT_PATIENTS, 1))
-#   ) %>%
-#   select(-SDC)
-# 
-# # Format factors etc and sort
-# patients_by_geography_and_gender_and_age_band_df <-
-#   patients_by_geography_and_gender_and_age_band_df %>%
-#   careHomePrescribingScrollytellR::format_data_raw(
-#     vars = c("GENDER", "AGE_BAND")
-#   )
-# 
-# # Add to data-raw/
-# usethis::use_data(
-#   patients_by_geography_and_gender_and_age_band_df,
-#   overwrite = TRUE
-# )
-# 
 # Disconnect from database
 DBI::dbDisconnect(con)
