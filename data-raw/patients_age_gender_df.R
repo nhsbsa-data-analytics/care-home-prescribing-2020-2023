ec6b28ce8e147b17f3a5d237fb8eaeccc321e647
mgarbuzov <92371047+mgarbuzov@users.noreply.github.com>
2023-06-12 19:15:41 +0100

    Clean ICB names

--- data-raw/patients_age_gender_df.R
+++ data-raw/patients_age_gender_df.R
@@ -105,7 +105,8 @@ patients_by_fy_geo_age_gender_df <-
   patients_by_fy_geo_age_gender_df |>
     mutate(SUB_GEOGRAPHY_NAME = stringr::str_remove(SUB_GEOGRAPHY_NAME, " \\(as of \\d{6}\\)$")) |>
     mutate(SUB_GEOGRAPHY_NAME = stringr::str_remove(SUB_GEOGRAPHY_NAME, " PCN$")) |>
-    mutate(SUB_GEOGRAPHY_NAME = stringr::str_remove(SUB_GEOGRAPHY_NAME, " ICB$"))
+    mutate(SUB_GEOGRAPHY_NAME = stringr::str_remove(SUB_GEOGRAPHY_NAME, " ICB$")) |>
+    mutate(SUB_GEOGRAPHY_NAME = stringr::str_remove(SUB_GEOGRAPHY_NAME, "^NHS "))
   
 
 # Add to data/
