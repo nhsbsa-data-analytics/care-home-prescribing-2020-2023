193f0852490e8a9e9aa87a20733e5b30029f6cfa
mgarbuzov <92371047+mgarbuzov@users.noreply.github.com>
2023-06-23 14:06:14 +0100

    Nicer formatting

--- R/mod_05_metrics_age_gender.R
+++ R/mod_05_metrics_age_gender.R
@@ -60,117 +60,113 @@ mod_05_metrics_age_gender_server <- function(id){
     )
     
     # Create chart
-    output$metrics_by_gender_and_age_band_and_ch_flag_chart <-
-      highcharter::renderHighchart({
-        req(input$gender_and_age_band_and_ch_flag_metric)
-        req(input$fy)
+output$metrics_by_gender_and_age_band_and_ch_flag_chart <- highcharter::renderHighchart({
+    req(input$gender_and_age_band_and_ch_flag_metric)
+    req(input$fy)
         
-        highcharter::highchart() %>%
-          highcharter::hc_add_series(
-            data = metrics_by_age_gender_and_ch_flag_df %>%
-              dplyr::filter(FY == input$fy & CH_FLAG == 1 & GENDER == "Female"),
-            type = "line",
-            highcharter::hcaes(
-              x = AGE_BAND,
-              y = .data[[input$gender_and_age_band_and_ch_flag_metric]]
-            ),
-            name = "Care home - Female",
-            color = "#003087",
-            marker = list(
-              symbol = stringr::str_glue("url({data_uri})", data_uri = female_ch),
-              radius = 2
-            ),
-            icon = female_ch
-          ) %>%
-          highcharter::hc_add_series(
-            data = metrics_by_age_gender_and_ch_flag_df %>%
-              dplyr::filter(FY == input$fy & CH_FLAG == 1 & GENDER == "Male"),
-            type = "line",
-            highcharter::hcaes(
-              x = AGE_BAND,
-              y = .data[[input$gender_and_age_band_and_ch_flag_metric]]
-            ),
-            name = "Care home - Male",
-            color = "#003087",
-            marker = list(
-              symbol = stringr::str_glue("url({data_uri})", data_uri = male_ch),
-              radius = 2
-            ),
-            icon = male_ch
-          ) %>%
-          highcharter::hc_add_series(
-            data = metrics_by_age_gender_and_ch_flag_df %>%
-              dplyr::filter(FY == input$fy, CH_FLAG == 0 & GENDER == "Female"),
-            type = "line",
-            highcharter::hcaes(
-              x = AGE_BAND,
-              y = .data[[input$gender_and_age_band_and_ch_flag_metric]]
-            ),
-            name = "Non-care home - Female",
-            color = "#768692",
-            marker = list(
-              symbol = stringr::str_glue("url({data_uri})", data_uri = female_non_ch),
-              radius = 2
-            ),
-            icon = female_non_ch
-          ) %>%
-          highcharter::hc_add_series(
-            data = metrics_by_age_gender_and_ch_flag_df %>%
-              dplyr::filter(FY == input$fy & CH_FLAG == 0 & GENDER == "Male"),
-            type = "line",
-            highcharter::hcaes(
-              x = AGE_BAND,
-              y = .data[[input$gender_and_age_band_and_ch_flag_metric]]
-            ),
-            name = "Non-care home - Male",
-            color = "#768692",
-            marker = list(
-              symbol = stringr::str_glue("url({data_uri})", data_uri = male_non_ch),
-              radius = 2
-            ),
-            icon = male_non_ch
-          ) %>%
-          theme_nhsbsa(stack = NA) %>%
-          highcharter::hc_yAxis(
-            min = 0,
-            title = list(
-              text = paste(
-                switch(input$gender_and_age_band_and_ch_flag_metric,
-                       "SDC_COST_PER_PATIENT_MONTH" = "Drug cost (£)",
-                       "SDC_ITEMS_PER_PATIENT_MONTH" =
-                         "Number of prescription items",
-                       "SDC_UNIQUE_MEDICINES_PER_PATIENT_MONTH" =
-                         "Number of unique medicines",
-                       "SDC_PCT_PATIENTS_TEN_OR_MORE_PER_PATIENT_MONTH" =
-                         "Patients on ten or more unique medicines (%)"
-                )
+    highcharter::highchart() |>
+      
+      highcharter::hc_add_series(
+        data = metrics_by_age_gender_and_ch_flag_df |>
+          dplyr::filter(FY == input$fy & CH_FLAG == 1 & GENDER == "Female"),
+          type = "line",
+          highcharter::hcaes(
+            x = AGE_BAND,
+            y = .data[[input$gender_and_age_band_and_ch_flag_metric]]
+          ),
+          name = "Care home - Female",
+          color = "#003087",
+          marker = list(
+            symbol = stringr::str_glue("url({data_uri})", data_uri = female_ch),
+            radius = 2
+          ),
+          icon = female_ch) |>
+      
+       highcharter::hc_add_series(
+         data = metrics_by_age_gender_and_ch_flag_df |>
+           dplyr::filter(FY == input$fy & CH_FLAG == 1 & GENDER == "Male"),
+         type = "line",
+         highcharter::hcaes(
+            x = AGE_BAND,
+            y = .data[[input$gender_and_age_band_and_ch_flag_metric]]
+          ),
+          name = "Care home - Male",
+          color = "#003087",
+          marker = list(
+            symbol = stringr::str_glue("url({data_uri})", data_uri = male_ch),
+            radius = 2
+          ),
+          icon = male_ch) |>
+      
+       highcharter::hc_add_series(
+         data = metrics_by_age_gender_and_ch_flag_df |>
+            dplyr::filter(FY == input$fy, CH_FLAG == 0 & GENDER == "Female"),
+          type = "line",
+          highcharter::hcaes(
+            x = AGE_BAND,
+            y = .data[[input$gender_and_age_band_and_ch_flag_metric]]
+          ),
+          name = "Non-care home - Female",
+          color = "#768692",
+          marker = list(
+            symbol = stringr::str_glue("url({data_uri})", data_uri = female_non_ch),
+            radius = 2
+          ),
+          icon = female_non_ch) |>
+      
+      highcharter::hc_add_series(
+        data = metrics_by_age_gender_and_ch_flag_df |>
+          dplyr::filter(FY == input$fy & CH_FLAG == 0 & GENDER == "Male"),
+        type = "line",
+        highcharter::hcaes(
+          x = AGE_BAND,
+          y = .data[[input$gender_and_age_band_and_ch_flag_metric]]
+        ),
+        name = "Non-care home - Male",
+        color = "#768692",
+        marker = list(
+          symbol = stringr::str_glue("url({data_uri})", data_uri = male_non_ch),
+          radius = 2
+        ),
+        icon = male_non_ch) |>
+      
+    theme_nhsbsa(stack = NA) |>
+      
+    highcharter::hc_yAxis(
+      min = 0,
+      title = list(
+      text = paste(
+      switch(input$gender_and_age_band_and_ch_flag_metric,
+             "SDC_COST_PER_PATIENT_MONTH" = "Drug cost (£)",
+             "SDC_ITEMS_PER_PATIENT_MONTH" = "Number of prescription items",
+             "SDC_UNIQUE_MEDICINES_PER_PATIENT_MONTH" = "Number of unique medicines",
+             "SDC_PCT_PATIENTS_TEN_OR_MORE_PER_PATIENT_MONTH" = "Patients on ten or more unique medicines (%)")
               )
             )
-          ) %>%
-          highcharter::hc_xAxis(
-            title = list(text = "Patient Age Band"),
-            categories = unique(metrics_by_age_gender_and_ch_flag_df$AGE_BAND)
-          ) %>%
-          highcharter::hc_tooltip(
-            shared = TRUE,
-            useHTML = TRUE,
-            valueDecimals = switch(input$gender_and_age_band_and_ch_flag_metric,
-                                   "SDC_PCT_PATIENTS_TEN_OR_MORE_PER_PATIENT_MONTH" = 1,
-                                   "SDC_UNIQUE_MEDICINES_PER_PATIENT_MONTH" = 1,
-                                   "SDC_ITEMS_PER_PATIENT_MONTH" = 1
-            ),
-            # valueDecimals = 1
-            headerFormat = "<b> {point.value:.1f} </b>",
-            valueSuffix = switch(input$gender_and_age_band_and_ch_flag_metric,
-                                 "SDC_PCT_PATIENTS_TEN_OR_MORE_PER_PATIENT_MONTH" = "%"
-            ),
-            valuePrefix = switch(input$gender_and_age_band_and_ch_flag_metric,
-                                 "SDC_COST_PER_PATIENT_MONTH" = "£"
-            )
-          )
-      })
-
+          ) |>
+      
+    highcharter::hc_xAxis(
+      title = list(text = "Patient Age Band"),
+      categories = unique(metrics_by_age_gender_and_ch_flag_df$AGE_BAND)
+      ) |>
+      
+    highcharter::hc_tooltip(
+      shared = TRUE,
+      useHTML = TRUE,
+      valueDecimals = switch(input$gender_and_age_band_and_ch_flag_metric,
+                             "SDC_PCT_PATIENTS_TEN_OR_MORE_PER_PATIENT_MONTH" = 1,
+                             "SDC_UNIQUE_MEDICINES_PER_PATIENT_MONTH" = 1,
+                             "SDC_ITEMS_PER_PATIENT_MONTH" = 1),
+      # valueDecimals = 1,
+      headerFormat = "<b> {point.value:.1f} </b>",
+      valueSuffix = switch(input$gender_and_age_band_and_ch_flag_metric,
+                           "SDC_PCT_PATIENTS_TEN_OR_MORE_PER_PATIENT_MONTH" = "%"),
+      valuePrefix = switch(input$gender_and_age_band_and_ch_flag_metric,
+                           "SDC_COST_PER_PATIENT_MONTH" = "£")
+      )
     
+    })
+
   })
 }
 
