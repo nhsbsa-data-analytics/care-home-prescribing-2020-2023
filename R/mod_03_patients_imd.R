fa32e94065f2985a613888ef10b38a087809fd44
AdnanS <adnan.shroufi@nhs.net>
2023-06-28 14:19:11 +0100

    mod3 text update

--- R/mod_03_patients_imd.R
+++ R/mod_03_patients_imd.R
@@ -1,4 +1,4 @@
-#' mod 01 headline figures function
+#' mod 03 patients imd function
 #'
 #' @description A shiny Module.
 #'
@@ -11,24 +11,24 @@ mod_03_patients_imd_ui <- function(id) {
   ns <- NS(id)
   tagList(
     h2(
-      "... IMD ..."
+      "There is a slight variation in numbers of care home patients aged 65 years or over by deprivation, with no overall trend."
     ),
     # Chart One
     
     nhs_card(
-      heading = "Annual and monthly total care home items prescribed, total cost of care home prescribing and distinct patients that received care home prescribing",
+      heading = "Deprivation Decile of care home patients aged 65 years or over in England per financial year",
       
       # Metric select input
       nhs_selectInput(
         inputId = ns("financial_year"),
         label = "Financial Year",
-        choices = c("Financial Year" = "Financial Year"),
+        choices = unique(carehomes2::mod_patients_by_imd_df$`Financial Year`),
         full_width = FALSE
       ),
       
       # Require unequal column widths
       fluidRow(
-        
+    
         # Chart
         column(
           12,
@@ -43,7 +43,11 @@ mod_03_patients_imd_ui <- function(id) {
       tags$text(
         class = "highcharts-caption",
         style = "font-size: 9pt",
-        "... IMD ..."
+        paste0(
+          "IMD deciles were attributed to care home address based on their postcode. ",
+          "Every distinct patient that recieved a prescription form from a care homw off given IMD decile contributed to their total. ",
+          "If patients moved between care homes they could potentially be counted in multiple IMD decile total distinct patient counts." 
+        )
       ),
       
       # Data download option
@@ -54,7 +58,7 @@ mod_03_patients_imd_ui <- function(id) {
   )
 }
 
-#' 02_demographics Server Functions
+#' mod 03 patienst imd Server Functions
 #'
 #' @noRd
 mod_03_patients_imd_server <- function(id, export_data) {
@@ -68,55 +72,34 @@ mod_03_patients_imd_server <- function(id, export_data) {
       req(input$financial_year)
       
       # Select and rename chosen column
-      carehomes2::mod_patients_imd_df %>% 
-        dplyr::filter(`Financial Year` = input$financial_year)
+      carehomes2::mod_patients_by_imd_df %>% 
+        dplyr::filter(`Financial Year` == input$financial_year)
     })
     
     # Annual Chart
     output$patients_imd_chart <- highcharter::renderHighchart({
       
-      headline_figures_df() %>% 
-        dplyr::filter(TYPE == "ANNUAL") %>% 
-        highcharter::hchart(., "column", highcharter::hcaes(TIME, METRIC, color = nhsbsaR::palette_nhsbsa()[1])) %>% 
-        highcharter::hc_xAxis(title = list(text = "<b> Financial Year</b>")) %>%  
-        highcharter::hc_yAxis(
-          min = 0,
-          title = list(
-            text = paste(
-              switch(
-                input$metric,
-                "PATS" = "<b>Annual Distinct Patients</b>",
-                "ITEMS" = "<b>Annual Total Items</b>",
-                "NIC" = "<b>Annual Total Cost (£)</b>"
-              )
-            )
-          )
-        ) %>% 
-        highcharter::hc_tooltip(
-          headerFormat = "",
-          pointFormat = paste0(
-            "<b>Year: </b> {point.TIME}<br>",
-            switch(
-              input$metric,
-              "PATS" = "<b>Distinct Patients: </b> {point.METRIC:,.0f}",
-              "ITEMS" = "<b>Total Items: </b> {point.METRIC:,.0f}",
-              "NIC" = "<b>Total Cost: </b> £{point.METRIC:,.0f}"
-            )
-          )
-        )
+      nhsbsaR::chart_hc_imd_bar(
+        df = imd_df(),
+        imd_col = "IMD Decile",
+        value_col = "Number of Patients",
+        metric_def = "Number of Patients",
+        highlight_core20 = TRUE,
+        value_dp = 0
+      )
     })
     
     # Add a download button
     mod_nhs_download_server(
       id = "download_patients_imd_chart",
       filename = "patient_imd.csv",
-      export_data = carehomes2::mod_patient_imd_df
+      export_data = carehomes2::mod_patient_by_imd_df
     )
   })
 }
 
 ## To be copied in the UI
-# mod_02_demographics_ui("02_demographics_ui_1")
+# mod_03_patients_imd_ui("03_patients_imd_ui_1")
 
 ## To be copied in the server
-# mod_02_demographics_server("02_demographics_ui_1")
+# mod_03_patients_imd_server("03_patients_imd_ui_1")
