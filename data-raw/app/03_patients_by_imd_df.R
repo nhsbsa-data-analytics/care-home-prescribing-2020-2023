1f37d7ad177eb4990a74d0c324418761e9c494b7
AdnanS <adnan.shroufi@nhsbsa.nhs.uk>
2023-07-12 16:09:45 +0100

    39 fine tuning of entire analysis content (#41)
    * falls risk eda plus mod1&3 update
    
    * mod7 edits
    
    * final changes to mod

--- data-raw/app/03_patients_by_imd_df.R
+++ data-raw/app/03_patients_by_imd_df.R
@@ -21,10 +21,11 @@ mod_patients_by_imd_df <- fact_db %>%
   ungroup() %>% 
   group_by(FY) %>% 
   # Sum so annual percentages add to 100 (despite double counting)
-  mutate(PCT_PATIENTS = janitor::round_half_up(TOTAL_PATIENTS / sum(TOTAL_PATIENTS) * 100, 2)) %>% 
+  mutate(PCT_PATIENTS = (TOTAL_PATIENTS / sum(TOTAL_PATIENTS)) * 100) %>% 
   ungroup() %>% 
-  collect() %>% 
+  nhsbsaR::collect_with_parallelism(., 16) %>% 
   arrange(FY, IMD_DECILE) %>% 
+  mutate(PCT_PATIENTS = janitor::round_half_up(PCT_PATIENTS, 2)) %>% 
   rename(
     `Financial Year` = FY,
     `IMD Decile` = IMD_DECILE,
