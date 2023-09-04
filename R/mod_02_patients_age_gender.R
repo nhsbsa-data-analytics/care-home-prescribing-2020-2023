2ac8758ccf039c6e0a0c853f5367e8d036b78356
mgarbuzov <92371047+mgarbuzov@users.noreply.github.com>
2023-09-04 12:21:29 +0100

    among -> of

--- R/mod_02_patients_age_gender.R
+++ R/mod_02_patients_age_gender.R
@@ -128,7 +128,7 @@ mod_02_patients_age_gender_server <- function(id){
             
           } else NULL,
           
-          " In each age band, patient counts of ≤5 were rounded up to the nearest 5, otherwise to the nearest 10; and the percentages are based on rounded counts. Hollow bars show percentages among non-care home patients."
+          " In each age band, patient counts of ≤5 were rounded up to the nearest 5, otherwise to the nearest 10; and the percentages are based on rounded counts. Hollow bars show percentages of non-care home patients."
               
         )
       )
