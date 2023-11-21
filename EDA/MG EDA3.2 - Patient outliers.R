4c84b7115d81194ed0fe17f66f448b6ccbff5f22
mgarbuzov <92371047+mgarbuzov@users.noreply.github.com>
2023-11-21 20:29:36 +0000

    Patient level - putatively finalised

--- EDA/MG EDA3.2 - Patient outliers.R
+++ EDA/MG EDA3.2 - Patient outliers.R
@@ -21,7 +21,9 @@ df1 <- DB |>
         CHAPTER_1_4_6_10_CAT == 1 ~ BNF_CHEMICAL_SUBSTANCE,
         TRUE ~ NA
       )
-    )
+    ),
+    
+    COST = sum(ITEM_PAY_DR_NIC)/100
     
   ) |>
   ungroup() |> 
@@ -30,7 +32,8 @@ df1 <- DB |>
     TOTAL_ITEMS = sum(TOTAL_ITEMS),
     TOTAL_MONTHS = n_distinct(YEAR_MONTH),
     MEAN_MONTHLY_ITEMS = sum(TOTAL_ITEMS) / n_distinct(YEAR_MONTH),
-    MEAN_MONTHLY_UNIQUE_MEDS = sum(UNIQUE_MEDICINES) / n_distinct(YEAR_MONTH)
+    MEAN_MONTHLY_UNIQUE_MEDS = sum(UNIQUE_MEDICINES) / n_distinct(YEAR_MONTH),
+    MEAN_MONTHLY_COST = sum(COST) / n_distinct(YEAR_MONTH)
   ) |>
   nhsbsaR::collect_with_parallelism(32)
 tictoc::toc()
@@ -62,7 +65,9 @@ df2 <- DB |>
         CHAPTER_1_4_6_10_CAT == 1 ~ BNF_CHEMICAL_SUBSTANCE,
         TRUE ~ NA
       )
-    )
+    ),
+    
+    COST = sum(ITEM_PAY_DR_NIC)/100
     
   ) |>
   ungroup() |>
@@ -79,17 +84,19 @@ df2 <- DB |>
   group_by(NHS_NO, AGE, TOTAL_MONTHS, SECTION_DESCR) |>
   summarise(
     TOTAL_ITEMS = sum(TOTAL_ITEMS),
-    SUM_MONTHLY_UNIQUE_MEDS = sum(UNIQUE_MEDICINES)
+    SUM_MONTHLY_UNIQUE_MEDS = sum(UNIQUE_MEDICINES),
+    TOTAL_COST = sum(COST)
   ) |>
   ungroup() |>
 
   mutate(
     MEAN_MONTHLY_ITEMS = TOTAL_ITEMS / TOTAL_MONTHS,
-    MEAN_MONTHLY_UNIQUE_MEDS = SUM_MONTHLY_UNIQUE_MEDS / TOTAL_MONTHS
+    MEAN_MONTHLY_UNIQUE_MEDS = SUM_MONTHLY_UNIQUE_MEDS / TOTAL_MONTHS,
+    MEAN_MONTHLY_COST = TOTAL_COST / TOTAL_MONTHS
   ) |>
   nhsbsaR::collect_with_parallelism(24)
 tictoc::toc()
-# Runnning time ~1.5 min
+# Running time ~1.5 min
 
 DBI::dbDisconnect(con); rm(con, DB)
 
@@ -110,7 +117,8 @@ highchart() |>
 
 
 df2 <- df2 |> group_by(NHS_NO) |>
-  mutate(ALL_MEAN_MONTHLY_ITEMS = sum(MEAN_MONTHLY_ITEMS)) |> # Window-rollup across all sections
+  mutate(ALL_MEAN_MONTHLY_ITEMS = sum(MEAN_MONTHLY_ITEMS),
+         MEAN_MONTHLY_COST_ALL_ITEMS = sum(MEAN_MONTHLY_COST)) |> # Window-rollup across all sections
   ungroup() |>
   arrange(-ALL_MEAN_MONTHLY_ITEMS)
 
@@ -136,8 +144,36 @@ highchart() |>
 
 
 
+df1 |>
+  ggplot(aes(MEAN_MONTHLY_COST)) + geom_histogram(binwidth = 5)
 
+highchart() |>
+  hc_add_series(
+    df2 |> filter(MEAN_MONTHLY_COST_ALL_ITEMS >= 2772), # Hacky way to get top5
+    "column",
+    hcaes(NHS_NO, MEAN_MONTHLY_COST, group = SECTION_DESCR),
+    stacking = "normal"
+  ) |>
+  hc_xAxis(
+    type = "category"
+  ) |>
+  #hc_yAxis(
+    #min = 0, max = 180
+  #) |>
+  hc_tooltip(
+    headerFormat = "",
+    pointFormat = "Age: {point.AGE}<br>{series.name}: {point.y}"
+  ) |>
+  hc_legend(enabled = F) 
 
+t <- df2 |> filter(NHS_NO == 4483170988)
+
+t <- tbl(con, in_schema("DALL_REF", "INT646_BASE_20200401_20230331")) |>
+  filter(NHS_NO == 4483170988,
+         SECTION_DESCR == "Drugs used in psychoses and related disorders",
+         FY == "2022/23",
+         CH_FLAG==1) |>
+  nhsbsaR::collect_with_parallelism(28)
 
 
 
