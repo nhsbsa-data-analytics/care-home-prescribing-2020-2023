29d7e72b20cd9c8e653bf097b0c9769e333a4665
Adnan Shroufi <adnan.shroufi@nhsbsa.nhs.uk>
2024-06-27 15:00:13 +0100

    sql file additions

--- data-raw/app/00_run_all.R
+++ data-raw/app/00_run_all.R
@@ -1,8 +1,12 @@
+Sys.time()
+# Load library and generate base geo data
 source("data-raw/app/data_raw_helpers.R")
 source("data-raw/app/geo_data.R")
 
+# Define vars to retain in workflow
 keep_vars = c(ls(), 'keep_vars', 'get_metrics')
 
+# Run all scripts that generate an Rda file
 source("data-raw/app/01_headline_figures_df.R")
 source("data-raw/app/02_patients_age_gender_df.R")
 source("data-raw/app/03_patients_by_imd_df.R")
@@ -11,3 +15,4 @@ source("data-raw/app/05_metrics_age_gender_df.R")
 source("data-raw/app/06_metrics_by_geo_and_ch_flag_df.R")
 source("data-raw/app/07_ch_flag_drug_df.R")
 source("data-raw/app/08_geo_ch_flag_drug_df.R")
+Sys.time()
---
+++ data-raw/workflow/workflow_union_4_years_in_one_table.sql
@@ -0,0 +1,53 @@
+-- To run from DALL_REF
+
+drop table int646_base_20200401_20240331 purge;
+create table int646_base_20200401_20240331 nologging compress for query low as
+
+select * from dall_ref.int646_base_20200401_20230331 
+union all
+select '2023/24' fy, y4.* from adnsh.int646_base_20230401_20240331 y4 
+;
+
+grant select on int646_base_20200401_20240331 to migar, adnsh, mamcp;
+
+create index int646_i01 on int646_base_20200401_20240331 (fy);
+create index int646_i02 on int646_base_20200401_20240331 (fy, year_month);
+
+create index int646_i03 on int646_base_20200401_20240331 (fy, age_band, gender);
+create index int646_i04 on int646_base_20200401_20240331 (fy, age_band, gender, pcd_region_code, pcd_region_name);
+create index int646_i05 on int646_base_20200401_20240331 (fy, age_band, gender, pcd_icb_code, pcd_icb_name);
+create index int646_i06 on int646_base_20200401_20240331 (fy, age_band, gender, pcd_lad_code, pcd_lad_name);
+create index int646_i07 on int646_base_20200401_20240331 (fy, imd_decile);
+create index int646_i08 on int646_base_20200401_20240331 (ch_flag);
+create index int646_i09 on int646_base_20200401_20240331 (fy, ch_flag);
+create index int646_i10 on int646_base_20200401_20240331 (fy, nursing_home_flag);
+create index int646_i11 on int646_base_20200401_20240331 (fy, residential_home_flag);
+
+create index int646_i12 on int646_base_20200401_20240331 (fy, ch_flag, pcd_region_code, pcd_region_name);
+create index int646_i13 on int646_base_20200401_20240331 (fy, ch_flag, pcd_icb_code, pcd_icb_name);
+create index int646_i14 on int646_base_20200401_20240331 (fy, ch_flag, pcd_lad_code, pcd_lad_name);
+
+create index int646_i15 on int646_base_20200401_20240331 (fy, chapter_descr, pcd_region_code, pcd_region_name);
+create index int646_i16 on int646_base_20200401_20240331 (fy, chapter_descr, pcd_icb_code, pcd_icb_name);
+create index int646_i17 on int646_base_20200401_20240331 (fy, chapter_descr, pcd_lad_code, pcd_lad_name);
+
+create index int646_i18 on int646_base_20200401_20240331 (fy, section_descr, pcd_region_code, pcd_region_name);
+create index int646_i19 on int646_base_20200401_20240331 (fy, section_descr, pcd_icb_code, pcd_icb_name);
+create index int646_i20 on int646_base_20200401_20240331 (fy, section_descr, pcd_lad_code, pcd_lad_name);
+
+create index int646_i21 on int646_base_20200401_20240331 (fy, paragraph_descr, pcd_region_code, pcd_region_name);
+create index int646_i22 on int646_base_20200401_20240331 (fy, paragraph_descr, pcd_icb_code, pcd_icb_name);
+create index int646_i23 on int646_base_20200401_20240331 (fy, paragraph_descr, pcd_lad_code, pcd_lad_name);
+
+create index int646_i24 on int646_base_20200401_20240331 (fy, chemical_substance_bnf_descr, pcd_region_code, pcd_region_name);
+create index int646_i25 on int646_base_20200401_20240331 (fy, chemical_substance_bnf_descr, pcd_icb_code, pcd_icb_name);
+create index int646_i26 on int646_base_20200401_20240331 (fy, chemical_substance_bnf_descr, pcd_lad_code, pcd_lad_name);
+
+create index int646_i27 on int646_base_20200401_20240331 (uprn_flag);
+
+create index int646_i28 on int646_base_20200401_20240331 (fy, year_month, nhs_no);
+create index int646_i29 on int646_base_20200401_20240331 (fy, year_month, pcd_region_code, pcd_region_name, nhs_no);
+create index int646_i30 on int646_base_20200401_20240331 (fy, year_month, pcd_icb_code, pcd_icb_name, nhs_no);
+create index int646_i31 on int646_base_20200401_20240331 (fy, year_month, pcd_lad_code, pcd_lad_name, nhs_no);
+
+create index int646_i32 on int646_base_20200401_20240331 (fy, age_band, gender, ch_flag, year_month, nhs_no);
