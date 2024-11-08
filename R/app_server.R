bbfb2a0a83a84e25f42f8c48d7a9749611233878
Mark McPherson <mark.mcpherson@nhsbsa.nhs.uk>
2024-11-08 12:31:48 +0000

    Update call to logging function

--- R/app_server.R
+++ R/app_server.R
@@ -5,10 +5,7 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-  shinyusertracking::set_user_tracking(
-    session,
-    columns = c("login", "logout", "duration")
-  )
+  shinyusertracking::set_user_tracking()
   
   mod_01_headline_figures_server("headline_figures")
   mod_02_patients_age_gender_server("patients_age_gender")
