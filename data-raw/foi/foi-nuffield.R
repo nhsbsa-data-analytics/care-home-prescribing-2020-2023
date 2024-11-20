c75b306deabd5c8a8592aa355bcd9624342b3c68
Mark McPherson <mark.mcpherson@nhsbsa.nhs.uk>
2024-11-20 16:45:10 +0000

    Yearly and monthly queries + total patients

--- data-raw/foi/foi-nuffield.R
+++ data-raw/foi/foi-nuffield.R
@@ -17,31 +17,115 @@ con <- nhsbsaR::con_nhsbsa(database = "DALP")
 base_db <- con %>%
   tbl(from = in_schema("DALL_REF", "INT646_BASE_20200401_20230331"))
 
-foi_df <- base_db %>% 
+
+## Query 1: Yearly --------------------------------------------------------
+
+# REQUEST:
+# For financial years from 2020/21 to 2022/23, provide totals of
+#     item count
+#     cost
+#     patients
+# aggregated by
+#     financial year
+#     age group
+#     sex
+#     care home setting (care-home or non-care home)
+#     deprivation
+#     BNF chapter
+
+foi_yearly_df <- base_db %>% 
   summarise(
     TOTAL_ITEMS = sum(ITEM_COUNT, na.rm = TRUE),
     TOTAL_COST = sum(ITEM_PAY_DR_NIC, na.rm = TRUE) / 100,
-    .by = c(CH_FLAG, AGE_BAND, GENDER, IMD_DECILE, CHAPTER_DESCR)
+    TOTAL_PATS = n_distinct(NHS_NO),
+    .by = c(
+      FY,
+      AGE_BAND,
+      GENDER,
+      CH_FLAG,
+      IMD_DECILE,
+      CHAPTER_DESCR
+    )
   ) %>% 
   collect()
 
-saveRDS(foi_df, "foi-nuffield.rds")
+saveRDS(foi_df, "foi-nuffield-yearly.rds")
 
-foi_final_df <- foi_df %>% 
+foi_yearly_final_df <- foi_yearly_df %>% 
   filter(!is.na(IMD_DECILE)) %>% 
   mutate(
+    TOTAL_PATS = case_when(
+      TOTAL_PATS < 5 ~ NA_integer_,
+      TRUE ~ TOTAL_PATS
+    ),
     TOTAL_ITEMS = case_when(
-      TOTAL_ITEMS < 5 ~ NA_integer_,
+      is.na(TOTAL_PATS) ~ NA_integer_,
       TRUE ~ TOTAL_ITEMS
     ),
     TOTAL_COST = case_when(
-      is.na(TOTAL_ITEMS) ~ NA_real_,
+      is.na(TOTAL_PATS) ~ NA_real_,
       TRUE ~ TOTAL_COST
     )
   )
 
-saveRDS(foi_final_df, "foi-final-nuffield.rds")
+saveRDS(foi_yearly_final_df, "foi-final-nuffield-yearly.rds")
 
-perc_redacted <- round(
-  100 * nrow(filter(foi_final_df, is.na(TOTAL_ITEMS))) / nrow(foi_final_df)
+perc_redacted_yearly <- round(
+  100 * nrow(filter(foi_yearly_final_df, is.na(TOTAL_PATS))) / nrow(foi_yearly_final_df)
+)
+
+## Query 1: Monthly --------------------------------------------------------
+
+# REQUEST:
+# For financial years from 2020/21 to 2022/23, provide totals of
+#     item count
+#     cost
+#     patients
+# aggregated by
+#     year_month
+#     age group
+#     sex
+#     care home setting (care-home or non-care home)
+#     deprivation
+#     BNF chapter
+
+foi_monthly_df <- base_db %>% 
+  summarise(
+    TOTAL_ITEMS = sum(ITEM_COUNT, na.rm = TRUE),
+    TOTAL_COST = sum(ITEM_PAY_DR_NIC, na.rm = TRUE) / 100,
+    TOTAL_PATS = n_distinct(NHS_NO),
+    .by = c(
+      YEAR_MONTH,
+      AGE_BAND,
+      GENDER,
+      CH_FLAG,
+      IMD_DECILE,
+      CHAPTER_DESCR
+    )
+  ) %>% 
+  collect()
+
+saveRDS(foi_df, "foi-nuffield-monthly.rds")
+
+foi_monthly_final_df <- foi_monthly_df %>% 
+  filter(!is.na(IMD_DECILE)) %>% 
+  mutate(
+    TOTAL_PATS = case_when(
+      TOTAL_PATS < 5 ~ NA_integer_,
+      TRUE ~ TOTAL_PATS
+    ),
+    TOTAL_ITEMS = case_when(
+      is.na(TOTAL_PATS) ~ NA_integer_,
+      TRUE ~ TOTAL_ITEMS
+    ),
+    TOTAL_COST = case_when(
+      is.na(TOTAL_PATS) ~ NA_real_,
+      TRUE ~ TOTAL_COST
+    )
+  )
+
+saveRDS(foi_monthly_final_df, "foi-final-nuffield-monthly.rds")
+
+perc_redacted_monthly <- round(
+  100 * nrow(filter(foi_monthly_final_df, is.na(TOTAL_PATS))) / nrow(foi_monthly_final_df)
 )
