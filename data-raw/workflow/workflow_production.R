37ada2c3a8b55b32f19bbcd73544547355e55845
MarkMc1089 <mark.mcpherson@nhsbsa.nhs.uk>
2025-05-30 11:34:54 +0100

    Refactor: Print run info always

--- data-raw/workflow/workflow_production.R
+++ data-raw/workflow/workflow_production.R
@@ -2,12 +2,10 @@
 #' @param none: date for data generated from Sys.date()
 #' @noRd
 get_latest_cqc_data = function(dry_run = FALSE){
-  if (dry_run) {
-    print("Running step 1 with:")
-    print(glue("cqc_date = {cqc_date}"))
-    
-    return(invisible())
-  }
+  print("Running step 1 with:")
+  print(glue("cqc_date = {cqc_date}"))
+  
+  if (dry_run) return(invisible())
   
   # Source single script with no date input required
   tictoc::tic()
@@ -20,12 +18,10 @@ get_latest_cqc_data = function(dry_run = FALSE){
 #' @param end_date: end date as a char in format 'YYYY-MM-DD'
 #' @noRd
 get_abp_from_api = function(dry_run = FALSE){
-  if (dry_run) {
-    print("Running step 2 with:")
-    print(glue("end_date = {end_date}"))
+  print("Running step 2 with:")
+  print(glue("end_date = {end_date}"))
     
-    return(invisible())
-  }
+  if (dry_run) return(invisible())
   
   # Get nearest ab plus to end date with cqc postcodes within time frame
   tictoc::tic()
@@ -38,12 +34,10 @@ get_abp_from_api = function(dry_run = FALSE){
 #' @param end_date: end date as a char in format 'YYYY-MM-DD'
 #' @noRd
 get_abp_from_os = function(dry_run = FALSE){
-  if (dry_run) {
-    print("Running step 2 with:")
-    print(glue("epoch_year = {epoch_year}"))
+  print("Running step 2 with:")
+  print(glue("epoch_year = {epoch_year}"))
     
-    return(invisible())
-  }
+  if (dry_run) return(invisible())
   
   # Get nearest ab plus to end date with cqc postcodes within time frame
   tictoc::tic()
@@ -56,12 +50,10 @@ get_abp_from_os = function(dry_run = FALSE){
 #' @param end_date: end date as a char in format 'YYYY-MM-DD'
 #' @noRd
 get_abp_from_dall_ref = function(dry_run = FALSE){
-  if (dry_run) {
-    print("Running step 2 with:")
-    print(glue("end_date = {end_date}"))
+  print("Running step 2 with:")
+  print(glue("end_date = {end_date}"))
     
-    return(invisible())
-  }
+  if (dry_run) return(invisible())
   
   # Get nearest ab plus to end date with cqc postcodes within time frame
   tictoc::tic()
@@ -75,15 +67,13 @@ get_abp_from_dall_ref = function(dry_run = FALSE){
 #' @param cqc_tbl: the name of the cqc db table
 #' @noRd
 create_ab_plus_cqc_data = function(dry_run = FALSE){
-  if (dry_run) {
-    print("Running step 3 with:")
-    print(glue("cqc_tbl = {cqc_tbl}"))
-    print(glue("abp_tbl = {abp_tbl}"))
-    print(glue("start_date = {start_date}"))
-    print(glue("end_date = {end_date}"))
+  print("Running step 3 with:")
+  print(glue("cqc_tbl = {cqc_tbl}"))
+  print(glue("abp_tbl = {abp_tbl}"))
+  print(glue("start_date = {start_date}"))
+  print(glue("end_date = {end_date}"))
     
-    return(invisible())
-  }
+  if (dry_run) return(invisible())
   
   # Stack and process the cqc and ab plus address data
   tictoc::tic()
@@ -96,12 +86,10 @@ create_ab_plus_cqc_data = function(dry_run = FALSE){
 #' @param address_tbl: name of the lookup address db table
 #' @noRd
 create_form_level_patient_addresses = function(dry_run = FALSE){
-  if (dry_run) {
-    print("Running step 4 with:")
-    print(glue("address_tbl = {address_tbl}"))
+  print("Running step 4 with:")
+  print(glue("address_tbl = {address_tbl}"))
     
-    return(invisible())
-  }
+  if (dry_run) return(invisible())
   
   # Join relevant fact tables and save as single table
   tictoc::tic()
@@ -115,14 +103,12 @@ create_form_level_patient_addresses = function(dry_run = FALSE){
 #' @param lookup_address_tbl: address data to be matched against
 #' @noRd
 create_care_home_address_match = function(dry_run = FALSE){
-  if (dry_run) {
-    print("Running step 5 with:")
-    print(glue("patient_tbl = {patient_tbl}"))
-    print(glue("address_tbl = {address_tbl}"))
-    print(glue("abp_tbl = {abp_tbl}"))
+  print("Running step 5 with:")
+  print(glue("patient_tbl = {patient_tbl}"))
+  print(glue("address_tbl = {address_tbl}"))
+  print(glue("abp_tbl = {abp_tbl}"))
     
-    return(invisible())
-  }
+  if (dry_run) return(invisible())
   
   # Perform address matching and save results
   tictoc::tic()
@@ -133,11 +119,9 @@ create_care_home_address_match = function(dry_run = FALSE){
 
 #' @description creates the postcode lookup table in the DB containing latest available (hard-coded) mappings
 create_postcode_lookup = function(dry_run = FALSE){
-  if (dry_run) {
-    print("Running step 6")
+  print("Running step 6")
     
-    return(invisible())
-  }
+  if (dry_run) return(invisible())
   
   # Write postcode lookup table to the DB for the appropriate FY
   tictoc::tic()
@@ -150,13 +134,11 @@ create_postcode_lookup = function(dry_run = FALSE){
 #' @param match_tbl: matched address data
 #' @noRd
 create_matched_prescription_base_table = function(dry_run = FALSE){
-  if (dry_run) {
-    print("Running step 7 with:")
-    print(glue("match_tbl = {match_tbl}"))
-    print(glue("patient_tbl = {patient_tbl}"))
+  print("Running step 7 with:")
+  print(glue("match_tbl = {match_tbl}"))
+  print(glue("patient_tbl = {patient_tbl}"))
     
-    return(invisible())
-  }
+  if (dry_run) return(invisible())
   
   # Create final base table for FY
   tictoc::tic()
