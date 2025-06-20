9e3762d5289c7632bd3c5bfab3c7f81fe8dd8b34
MarkMc1089 <mark.mcpherson@nhsbsa.nhs.uk>
2025-06-20 16:15:30 +0100

    Refactor: add data validation + some additional comments Validation: checks: * base table FY and YEAR_MONTH is as expected * calculates expected row count * no NAs in introduced columns and final row count as expected Comments: added to explain table union for age bands

--- data-raw/app/04_metrics_by_ch_type_85_split_df.R
+++ data-raw/app/04_metrics_by_ch_type_85_split_df.R
@@ -6,6 +6,13 @@ library(dbplyr)
 library(tidyr)
 devtools::load_all()
 
+
+base_table <- "INT646_BASE_20200401_20250331"
+start_year <- substring(base_table, 13, 16)
+end_year <- substring(base_table, 22, 25)
+EXPECTED_YEARS <- as.integer(end_year) - as.integer(start_year)
+EXPECTED_MONTHS <- 12 * EXPECTED_YEARS
+
 # Set up connection to DALP
 con <- nhsbsaR::con_nhsbsa(database = "DALP")
 
@@ -15,7 +22,17 @@ con <- nhsbsaR::con_nhsbsa(database = "DALP")
 
 # Item-level base table
 base_db <- con %>%
-  tbl(from = in_schema("DALL_REF", "INT646_BASE_20200401_20240331"))
+  tbl(from = in_schema("DALL_REF", base_table)) %>%
+  verify(nrow.alt(distinct(., FY)) == EXPECTED_YEARS) %>% 
+  verify(nrow.alt(distinct(., YEAR_MONTH)) == EXPECTED_MONTHS)
+
+
+# Row validation calculation ----------------------------------------------
+
+EXPECTED_ROWS <- EXPECTED_YEARS *
+  4 * # Categories are CH, NON_CH, RH or NH
+  2   # Have 2 age band groupings, 65+ and 85+
+
 
 # Initial manipulation to create CH_TYPE column, later to be grouped by
 init_db <- base_db %>%
@@ -27,7 +44,7 @@ init_db <- base_db %>%
     ends_with("FLAG"),
     names_to = "CH_TYPE"
   ) %>%
-  # Only keep rows where value is 1, if it is not then it is not the carehome
+  # Only keep rows where value is 1, if it is not then it is not the care home
   # type in that row
   filter(value == 1L) %>%
   # We only need the pivoted col names going forward
@@ -46,12 +63,12 @@ init_db <- base_db %>%
     )
   ) 
 
-# Union both initi_db variants
-init_db <- init_db %>% 
+# Union both age band variants
+init_db <- init_db %>%            # Start with all patients and flag for age band
   union(
     init_db %>% 
-      filter(AGE_GTE_85 == 1) %>% 
-      mutate(AGE_GTE_85 = 0)
+      filter(AGE_GTE_85 == 1) %>% # Get just those flagged as 85+
+      mutate(AGE_GTE_85 = 0)      # And unflag, effectively including them in 65+ band
   )
 
 ## Process ----------------------------------------------------------------
@@ -83,12 +100,30 @@ metrics_by_ch_type_85_split_df <- metrics_by_ch_type_85_split_df %>%
     ),
     AGE_GTE_85 = NULL
   ) %>% 
-  dplyr::relocate(AGE_BAND, .after = CH_TYPE)
+  dplyr::relocate(AGE_BAND, .after = CH_TYPE) %>%
+  verify(nrow.alt(.) == EXPECTED_ROWS) %>% 
+  assert.alt(
+    not_na.alt,
+    CH_TYPE,
+    AGE_BAND,
+    ITEMS_PPM,
+    COST_PPM,
+    UNIQ_MEDS_PPM,
+    UNIQ_MEDS_FALLS_PPM,
+    TOTAL_PM,
+    TOTAL_PM_ACB,
+    TOTAL_PM_DAMN,
+    PCT_PM_GTE_SIX,
+    PCT_PM_GTE_TEN,
+    PCT_PM_ACB,
+    PCT_PM_DAMN,
+    PCT_PM_FALLS
+  )
   
 ## Save ------------------------------------------------------------------------
 usethis::use_data(metrics_by_ch_type_85_split_df, overwrite = TRUE)
 
-# Cleanup ----------------------------------------------------------------------
+# Clean-up ----------------------------------------------------------------------
 
 # Disconnect
 DBI::dbDisconnect(con)
