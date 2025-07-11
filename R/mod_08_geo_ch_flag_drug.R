9cef505c8a9242cd0e0501670c8986064b9b52e1
MarkMc1089 <mark.mcpherson@nhsbsa.nhs.uk>
2025-07-11 11:08:58 +0100

    Refactor: Rename FY columns and build table columns dynamically

--- R/mod_08_geo_ch_flag_drug.R
+++ R/mod_08_geo_ch_flag_drug.R
@@ -456,7 +456,7 @@ mod_08_geo_ch_flag_drug_server <- function(id, export_data) {
 
     # Region: df after 4 initial filters applied
     region_df = reactive({
-      # Filter, pivot an rename
+      # Filter, pivot and rename
       df <- carehomes2::mod_geo_ch_flag_drug_df %>%
         dplyr::select(-PATS) %>%
         dplyr::filter(
@@ -469,11 +469,9 @@ mod_08_geo_ch_flag_drug_server <- function(id, export_data) {
         dplyr::select(
           GEOGRAPHY_PARENT,
           GEOGRAPHY_CHILD,
-          `20/21` = `2020/21`,
-          `21/22` = `2021/22`,
-          `22/23` = `2022/23`,
-          `23/24` = `2023/24`
+          dplyr::starts_with("20")
         ) %>%
+        dplyr::rename_with(\(x) gsub("^20", "", x)) %>% 
         dplyr::arrange(GEOGRAPHY_CHILD)
 
       if (startsWith(input$input_region_metric, "Total")) {
@@ -504,11 +502,9 @@ mod_08_geo_ch_flag_drug_server <- function(id, export_data) {
         dplyr::select(
           GEOGRAPHY_PARENT,
           GEOGRAPHY_CHILD,
-          `20/21` = `2020/21`,
-          `21/22` = `2021/22`,
-          `22/23` = `2022/23`,
-          `23/24` = `2023/24`
+          dplyr::starts_with("20")
         ) %>%
+        dplyr::rename_with(\(x) gsub("^20", "", x)) %>% 
         dplyr::arrange(GEOGRAPHY_CHILD)
 
       if (startsWith(input$input_ics_metric, "Total")) {
@@ -539,11 +535,9 @@ mod_08_geo_ch_flag_drug_server <- function(id, export_data) {
         dplyr::select(
           GEOGRAPHY_PARENT,
           GEOGRAPHY_CHILD,
-          `20/21` = `2020/21`,
-          `21/22` = `2021/22`,
-          `22/23` = `2022/23`,
-          `23/24` = `2023/24`
+          dplyr::starts_with("20")
         ) %>%
+        dplyr::rename_with(\(x) gsub("^20", "", x)) %>% 
         dplyr::arrange(GEOGRAPHY_CHILD)
 
       if (startsWith(input$input_lad_metric, "Total")) {
@@ -562,6 +556,15 @@ mod_08_geo_ch_flag_drug_server <- function(id, export_data) {
 
     # Function for each table
     geo_table = function(df, df_select, metric, geo_name){
+      # reactable is limited when setting col widths based on content, e.g.
+      # numeric columns to be certain width is not possible
+      # https://github.com/glin/reactable/issues/399
+      # Instead, can build columns beforehand
+      fys <- names(dplyr::select(df, -(1:2)))
+      columns <- purrr::map(fys, \(x) reactable::colDef(width = 70)) %>%
+        stats::setNames(fys)
+      columns <- c(list(.selection = reactable::colDef(width = 15)), columns)
+      
       df %>%
         dplyr::rename_at("GEOGRAPHY_CHILD", ~geo_name) %>%
         dplyr::select(-GEOGRAPHY_PARENT) %>%
@@ -574,13 +577,7 @@ mod_08_geo_ch_flag_drug_server <- function(id, export_data) {
           striped = TRUE,
           highlight = TRUE,
           borderless = FALSE,
-          columns = list(
-            .selection = reactable::colDef(width = 15),
-            `20/21` = reactable::colDef(width = 70),
-            `21/22` = reactable::colDef(width = 70),
-            `22/23` = reactable::colDef(width = 70),
-            `23/24` = reactable::colDef(width = 70)
-          ),
+          columns = columns,
           defaultColDef = reactable::colDef(
             headerClass = "my-header",
             cell = function(val, row, col_name) {
