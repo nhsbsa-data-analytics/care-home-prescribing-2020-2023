1863e3410650f4c362ffb2b4a4bfc4fd609f9bb1
MarkMc1089 <mark.mcpherson@nhsbsa.nhs.uk>
2025-07-23 17:53:31 +0100

    Refactor: Moves function from global to utils

--- R/global.R
+++ R/global.R
@@ -1,21 +1,2 @@
 # A global.R is the cleanest way of loading in some data before the UI is run
 latest_figures <- yaml::read_yaml("data/latest_figures.yaml")
-
-# This interpolates the values into the markdown
-# 
-# NOTE: the placeholders as written in the markdown are
-# £> and <£ - when glue gets these it will be in HTML form, thus why different
-# in this function
-# 
-# This would go in utils function file or similar
-include_dynamic_md <- function(md_path) {
-  # print(md_path)
-  # if(md_path == "inst/markdown/01_headline_figures_2.md") browser()
-  HTML(
-    glue::glue(
-      shiny::includeMarkdown(md_path),
-      .open = "£&gt;",
-      .close = "&lt;£"
-    )
-  )
-}
