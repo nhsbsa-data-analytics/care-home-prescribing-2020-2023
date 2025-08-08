f4b074f08ff83f89d5abbd90ae03df83f3edfbbd
MarkMc1089 <mark.mcpherson@nhsbsa.nhs.uk>
2025-08-08 09:59:33 +0100

    Revert "Feat: Replaces use of Apache license with OGL (#125)"
    This reverts commit 6f80fc936fcf273397d23c4001d357c479f36434.

--- R/nhs_footer.R
+++ R/nhs_footer.R
@@ -12,7 +12,6 @@ nhs_footer <- function() {
         id = "nhsuk-footer",
         tags$div(
           class = "nhsuk-width-container app-width-container",
-          style = "display: flex; align-items: center;",
           tags$ul(
             class = "nhsuk-footer__list",
             tags$li(
@@ -48,26 +47,9 @@ nhs_footer <- function() {
               )
             )
           ),
-          div(
+          p(
             class = "nhsuk-footer__copyright",
-            style = "display: inline-flex; flex-direction: column; width: 30%;",
-            tags$image(
-              class = "nhsuk-logo",
-              style = "height: 30px;",
-              src = "www/assets/logos/logo-ogl.svg",
-              name = "Open Government License logo",
-              alt = "Open Government License"
-            ),
-            span(
-              "All content is available under the",
-              style = "text-align: center;",
-              tags$a(
-                "Open Government Licence v3.0",
-                href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/",
-                rel = "license"
-              ),
-              "except where otherwise stated"
-            )
+            HTML("&#169; APLv2")
           )
         )
       )
