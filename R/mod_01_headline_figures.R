76b4f27d2892a017ce71430cfb8bc55b11f45582
MarkMc1089 <mark.mcpherson@nhsbsa.nhs.uk>
2025-10-30 17:09:48 +0000

    Feat: Adds geographic breakdown to mod 1 - Overall included, so initial view is the same as it was - Lost of refactoring of the data script - Overhauled the module code, to be similar to mod 4 and 6

--- R/mod_01_headline_figures.R
+++ R/mod_01_headline_figures.R
@@ -1,58 +1,64 @@
-#' mod 01 headline figures function
-#'
-#' @description A shiny Module.
-#'
-#' @param id,input,output,session Internal parameters for {shiny}.
-#'
-#' @noRd
-#'
-#' @importFrom shiny NS tagList
 mod_01_headline_figures_ui <- function(id) {
   ns <- NS(id)
+
   tagList(
     include_dynamic_md("inst/markdown/01_headline_figures_1.md"),
     tags$div(style = "margin-top: 25vh"),
     include_dynamic_md("inst/markdown/01_headline_figures_2.md"),
     nhs_card(
-      heading = "Estimated number of patients, prescription items and drug cost 
+      heading = "Estimated number of patients, prescription items and drug cost
                 for care home patients aged 65 years and over in England",
 
-      # Metric select input
-      nhs_selectInput(
-        inputId = ns("metric"),
-        label = "Metric",
-        choices = c(
-          "Patient count" = "PATS",
-          "Total prescription items" = "ITEMS",
-          "Total drug cost (£)" = "NIC",
-          "Item % for care homes" = "ITEMS_PERC",
-          "Cost % for care homes" = "NIC_PERC"
+      # Select inputs
+      nhs_grid_3_col(
+        nhs_selectInput(
+          inputId = ns("metric"),
+          label = "Metric",
+          choices = c(
+            "Patient count" = "PATS",
+            "Total prescription items" = "ITEMS",
+            "Total drug cost (£)" = "NIC",
+            "Item % for care homes" = "ITEMS_PERC",
+            "Cost % for care homes" = "NIC_PERC"
+          ),
+          full_width = FALSE
         ),
-        full_width = FALSE
+        nhs_selectInput(
+          inputId = ns("geography"),
+          label = "Geography",
+          choices = levels(carehomes2::mod_headline_figures_df$GEOGRAPHY),
+          full_width = FALSE
+        ),
+        nhs_selectInput(
+          inputId = ns("sub_geography"),
+          label = "Sub Geography",
+          choices = "Overall", # initial value, dynamically updated
+          full_width = FALSE
+        )
       ),
-      
+
       # Require unequal column widths
       fluidRow(
-        
+
         # First chart
         column(
           4,
           highcharter::highchartOutput(
             outputId = ns("headline_annual_chart"),
             height = "240px"
-            )
-          ),
-        
-        # Second chart       
+          )
+        ),
+
+        # Second chart
         column(
           8,
           highcharter::highchartOutput(
             outputId = ns("headline_monthly_chart"),
             height = "250px"
-            )
           )
-        ),
-      
+        )
+      ),
+
       # Chart caption
       tags$text(
         class = "highcharts-caption",
@@ -65,68 +71,119 @@ mod_01_headline_figures_ui <- function(id) {
   )
 }
 
-#' mod 01 headline figures Server Functions
-#'
-#' @noRd
 mod_01_headline_figures_server <- function(id, export_data) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
-    
-    # Filter the data based on the breakdown
-    headline_figures_df <- reactive({
-      
-      # Ensure select input required
-      req(input$metric)
-      
-      # Select and rename chosen column
-      carehomes2::mod_headline_figures_df %>%
-        dplyr::select('TYPE', 'TIME', input$metric) %>% 
-        dplyr::rename_at(input$metric, ~"METRIC")
+
+    # Metric name mappings ------------------------------------------------
+
+    # Map metric column names to UI metric names
+    ui_metric_names <- c(
+      PATS = "Mean monthly patient count",
+      ITEMS = "Mean monthly items",
+      NIC = "Mean monthly cost (£)",
+      ITEMS_PERC = "Item % for care homes",
+      NIC_PERC = "Cost % for care homes"
+    )
+
+    # Map metric column names to tooltip metric names
+    metric_tooltips <- c(
+      PATS = "<b>Mean monthly patients: </b> {point.METRIC:,.0f}",
+      ITEMS = "<b>Mean monthly items: </b> {point.METRIC:,.0f}",
+      NIC = "<b>Mean monthly cost: </b> £{point.METRIC:,.0f}",
+      ITEMS_PERC = "<b>Item % for care homes: {point.METRIC:,.1f}</b>",
+      NIC_PERC = "<b>Cost % for care homes: {point.METRIC:,.1f}</b>"
+    )
+
+    # Map all column names to download data names
+    dl_col_names <- c(
+      rlang::set_names(names(ui_metric_names), unname(ui_metric_names)),
+      "Time period" = "TIME",
+      "Metric type" = "TYPE",
+      "Geography" = "GEOGRAPHY",
+      "Sub-geography code" = "SUB_GEOGRAPHY_CODE",
+      "Sub-geography name" = "SUB_GEOGRAPHY_NAME"
+    )
+
+    # Formatted data ------------------------------------------------------
+
+    fmt_data <- carehomes2::mod_headline_figures_df %>%
+      dplyr::filter(.data$SUB_GEOGRAPHY_NAME != "Isles of Scilly") %>% 
+      # @Adnan - Probably temporary, but formatting here until we discuss
+      mutate(
+        # Patients nearest 100, Items 1,000, Cost 10,000
+        PATS = janitor::round_half_up(PATS, -2),
+        ITEMS = janitor::round_half_up(ITEMS, -3),
+        NIC = janitor::round_half_up(NIC, -4),
+        across(ends_with("PERC"), \(x) round(100 * x, 1))
+      )
+
+    # Reactive data -------------------------------------------------------
+
+    fdata <- reactive({
+      req(input$sub_geography)
+
+      fmt_data %>%
+        dplyr::filter(
+          .data$GEOGRAPHY == isolate(input$geography),
+          .data$SUB_GEOGRAPHY_NAME == input$sub_geography
+        ) %>%
+        dplyr::mutate(
+          .data$TIME,
+          .data$TYPE,
+          .data$GEOGRAPHY,
+          .data$SUB_GEOGRAPHY_NAME,
+          METRIC = .data[[input$metric]],
+          .keep = "none"
+        )
     })
-    
-    # Annual Chart
-    output$headline_annual_chart <- highcharter::renderHighchart({
-      headline_figures_df() %>% 
-        dplyr::filter(TYPE == "Annual") %>% 
-        highcharter::hchart(., "column", highcharter::hcaes(TIME, METRIC, color = nhsbsaR::palette_nhsbsa()[1])) %>% 
-        highcharter::hc_xAxis(title = list(text = "")) %>%  
+
+    # Output functions ----------------------------------------------------
+
+    # Annual chart
+    create_headline_annual_chart <- function(data) {
+      data %>%
+        dplyr::filter(.data$TYPE == "Annual") %>%
+        highcharter::hchart(
+          "column",
+          highcharter::hcaes(
+            .data$TIME,
+            .data$METRIC,
+            color = nhsbsaR::palette_nhsbsa()[1]
+          )
+        ) %>%
+        highcharter::hc_xAxis(title = list(text = "")) %>%
         highcharter::hc_yAxis(
           min = 0,
           title = list(
-            text = paste(
-              switch(
-                input$metric,
-                "PATS" = "<b>Mean monthly patient count</b>",
-                "ITEMS" = "<b>Mean monthly items</b>",
-                "NIC" = "<b>Mean monthly cost (£)</b>",
-                "ITEMS_PERC" = "<b>Item % for care homes</b>",
-                "NIC_PERC" = "<b>Cost % for care homes</b>"
-              )
-            )
+            text = ui_metric_names[input$metric] %>% unname(),
+            style = list(fontWeight = "bold")
           )
-        ) %>% 
+        ) %>%
         highcharter::hc_tooltip(
+          useHTML = TRUE,
           headerFormat = "",
           pointFormat = paste0(
-            "<b>Year: </b> {point.TIME}<br>",
-            switch(
-              input$metric,
-              "PATS" = "<b>Mean monthly patients: </b> {point.METRIC:,.0f}",
-              "ITEMS" = "<b>Mean monthly items: </b> {point.METRIC:,.0f}",
-              "NIC" = "<b>Mean monthly cost: </b> £{point.METRIC:,.0f}",
-              "ITEMS_PERC" = "<b>Item % for care homes: {point.METRIC:,.1f}</b>",
-              "NIC_PERC" = "<b>Cost % for care homes: {point.METRIC:,.1f}</b>"
-            )
+            tags$b("{point.TIME}", ": "), "{point.SUB_GEOGRAPHY_NAME}",
+            tags$br(),
+            metric_tooltips[input$metric] %>% unname()
           )
-        ) %>% 
+        ) %>%
         nhsbsaR::theme_nhsbsa_highchart()
-    })
-    
-    # Monthly Chart
-    output$headline_monthly_chart <- highcharter::renderHighchart({
-      headline_figures_df() %>% 
-        dplyr::filter(TYPE == "Monthly") %>% 
-        highcharter::hchart(., "line", highcharter::hcaes(TIME, METRIC, color = nhsbsaR::palette_nhsbsa()[1])) %>% 
+    }
+
+    # Monthly chart
+    create_headline_monthly_chart <- function(data) {
+      data %>%
+        dplyr::filter(.data$TYPE == "Monthly") %>%
+        highcharter::hchart(
+          "line",
+          highcharter::hcaes(
+            .data$TIME,
+            .data$METRIC,
+            color = nhsbsaR::palette_nhsbsa()[1]
+          )
+        ) %>%
         highcharter::hc_xAxis(
           title = list(text = ""),
           plotBands = list(list(
@@ -135,78 +192,81 @@ mod_01_headline_figures_server <- function(id, export_data) {
             to = 20,
             color = "#f0f0f0"
           ))
-        ) %>% 
+        ) %>%
         highcharter::hc_yAxis(
           min = 0,
           title = list(
-            text = paste(
-              switch(
-                input$metric,
-                "PATS" = "<b>Patient count</b>",
-                "ITEMS" = "<b>Items</b>",
-                "NIC" = "<b>Cost (£)</b>",
-                "ITEMS_PERC" = "<b>Item % for care homes</b>",
-                "NIC_PERC" = "<b>Cost % for care homes</b>"
-                )
-              )
-            )
-          ) %>% 
+            text = ui_metric_names[input$metric] %>% unname(),
+            style = list(fontWeight = "bold")
+          )
+        ) %>%
         highcharter::hc_tooltip(
+          useHTML = TRUE,
           headerFormat = "",
           pointFormat = paste0(
-            "<b>Month: </b> {point.TIME}<br>",
-            switch(
-              input$metric,
-              "PATS" = "<b>Patient count: </b> {point.METRIC:,.0f}",
-              "ITEMS" = "<b>Items: </b> {point.METRIC:,.0f}",
-              "NIC" = "<b>Cost: </b> £{point.METRIC:,.0f}",
-              "ITEMS_PERC" = "<b>Item % for care homes: {point.METRIC:,.1f}</b>",
-              "NIC_PERC" = "<b>Cost % for care homes: {point.METRIC:,.1f}</b>"
-              )
-            )
-          ) %>% 
+            tags$b("Month:"), "{point.TIME}",
+            tags$br(),
+            metric_tooltips[input$metric] %>% unname()
+          )
+        ) %>%
         nhsbsaR::theme_nhsbsa_highchart()
-    })
-    
-    # Create download data
+    }
+
+    # Create download data (all data)
     create_download_data <- function(data) {
       data %>%
-        dplyr::rename(
-          `Time period` = TIME,
-          `Metric type` = TYPE,
-          `Patient count` = PATS,
-          `Total prescription items` = ITEMS,
-          `Total drug cost` = NIC,
-          `Item % for care homes` = ITEMS_PERC,
-          `Cost % for care homes` = NIC_PERC
-        )
+        dplyr::rename(dl_col_names)
     }
-    
+
+    # Outputs -------------------------------------------------------------
+
+    # Charts
+    output$headline_annual_chart <- highcharter::renderHighchart({
+      create_headline_annual_chart(fdata())
+    })
+    output$headline_monthly_chart <- highcharter::renderHighchart({
+      create_headline_monthly_chart(fdata())
+    })
+
     # Download button
     mod_nhs_download_server(
       id = "download_data",
       filename = "Headline figures for care home prescribing.xlsx",
-      export_data = create_download_data(carehomes2::mod_headline_figures_df),
-      number_xl_fmt_str = "#,##0",
-      percent_xl_fmt_str = "#0.0%"
+      export_data = create_download_data(fmt_data),
+      number_xl_fmt_str = "#,##0"
     )
-    
+
+    # Reactive events -----------------------------------------------------
+
     observeEvent(
-      carehomes2::mod_headline_figures_df,
-      once = TRUE, {
-        req(carehomes2::mod_headline_figures_df)
-        
+      input$geography,
+      {
+        # ...get the unique sub-geographies for that geography...
+        choices <- fmt_data %>%
+          dplyr::filter(.data$GEOGRAPHY == input$geography) %>%
+          dplyr::pull(.data$SUB_GEOGRAPHY_NAME) %>%
+          unique()
+
+        # ...and update the sub_geography selectInput with these new choices.
+        updateSelectInput(
+          inputId = "sub_geography",
+          choices = choices
+        )
+      }
+    )
+
+    observeEvent(
+      fmt_data,
+      once = TRUE,
+      {
+        req(fmt_data)
+
         insertUI(
           selector = ".nhsuk-card__description:eq(0)",
           where = "beforeEnd",
           ui = mod_nhs_download_ui(ns("download_data"))
         )
-      })
+      }
+    )
   })
 }
-
-## To be copied in the UI
-# mod_02_demographics_ui("02_demographics_ui_1")
-
-## To be copied in the server
-# mod_02_demographics_server("02_demographics_ui_1")
